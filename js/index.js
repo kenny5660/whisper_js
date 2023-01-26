9d9b16bae53d80dd095de7506a4127c697d94d35
Mikhail Liamaev <32442117+kenny5660@users.noreply.github.com>
2023-01-26 12:20:51 +1000

    add App class

--- js/index.js
+++ js/index.js
@@ -17,6 +17,41 @@
 
 import * as tf from '@tensorflow/tfjs';
 import * as loader from './weights_loader';
-import * as ui from './ui';
 import * as ui from 'material-design-lite';
 import 'material-icons/iconfont/material-icons.css';
+
+class App{
+
+    constructor() {
+        this.gui_select_model = document.getElementById("select-model");
+        this.gui_model_progressbar = document.getElementById("model-progressbar");
+        this.gui_model_status = document.getElementById("model-status");
+        this.gui_select_model.onchange = () => this.onchangeSelectModel(this.gui_select_model)
+        self.mdlProgressInitDone = false;
+        this.counter = 0;
+        this.gui_model_progressbar.addEventListener('mdl-componentupgraded', function () {
+            console.log("componentupgraded");
+            this.MaterialProgress.setProgress(0);
+            self.mdlProgressInitDone = true;
+          });
+        this.gui_model_progressbar.nativeElement.MaterialProgress.setProgress(this.this.counter++)
+    }
+    
+    onchangeSelectModel(obj) {
+        console.log("Select model :", obj.value);
+        this.downloadModel(obj.value);
+    }
+    updateProgressBar() {
+        if (self.mdlProgressInitDone) {
+            console.log("update");
+            this.gui_model_progressbar.MaterialProgress.setProgress(this.counter++)
+        }
+    }
+    downloadModel(model) {
+        console.log("Download model :", model);
+        window.setInterval(() => this.updateProgress(), 200);
+        
+    }
+}
+
+var app = new App()
