9cb1b8515564f1f6df0e12635640b75024fcaa5e
Mikhail Liamaev <32442117+kenny5660@users.noreply.github.com>
2023-01-26 13:56:22 +1000

    fix python

--- python/weights_converter/setup.py
+++ python/weights_converter/setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
-import convert_weights
-
+import weights_converter
 setup(
     name='convert_weights',
     version='1.0.0',
@@ -8,11 +7,11 @@ setup(
             'tqdm==4.64.1',
             'urllib3==1.26.13',
             'numpy==1.23.5',
-            'torch==1.13.0+cu117',
+            'torch==1.13.0',
             'h5py==3.7.0'
         ],
         entry_points={
     'console_scripts':
-        ['convert_weights = convert_weights.convert_weights']
+        ['convert_weights = weights_converter.convert_weights']
     }
 )
