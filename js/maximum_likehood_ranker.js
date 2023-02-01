2013d572a7027f4e6667f2797fa350b06c5e6ce1
Alexey Winchester <86226273+winchesterdeeplay@users.noreply.github.com>
2023-02-02 00:57:15 +0800

    Fix typo

--- js/maximum_likehood_ranker.js
+++ js/maximum_likehood_ranker.js
@@ -1,6 +1,6 @@
 import * as tf from '@tensorflow/tfjs';
 
-
+/*
 Выбирает сэмплы с наибольшой вероятностью.
 Вероятности нормализуются на длину токенов или по правилу Google NMT.
 from the Google NMT paper: penalty = ((5 + length) / 6) ** self.length_penalty
